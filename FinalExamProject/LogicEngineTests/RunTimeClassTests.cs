d4f7c7fd4b70334f27c8dc09318ce6c71044f25c
nkarl <21993921+nkarl@users.noreply.github.com>
2022-04-29 09:24:49 -0700

    Made some changes to nunit test files

--- FinalExamProject/LogicEngineTests/RunTimeClassTests.cs
+++ FinalExamProject/LogicEngineTests/RunTimeClassTests.cs
@@ -1,6 +1,5 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using LogicEngine.RunTimeClass;
-using LogicEngine.RunTimeClass.FoodItem;
 
 namespace LogicEngineTests
 {
@@ -9,38 +8,6 @@ namespace LogicEngineTests
         [SetUp]
         public void Setup()
         {
-        }
-
-        [Test]
-        public void Test_VeggieObjectNotNull()
-        {
-            Veggie veggie = new Veggie();
-            
-            Assert.That(veggie, Is.Not.Null);
-        }
-
-        [Test]
-        public void Test_IsVeggieObject()
-        {
-            var veggie = new Veggie();
-
-            Assert.That(veggie, Is.InstanceOf<Veggie>());
-        }
-
-        [Test]
-        public void Test_FruitObjectNotNull()
-        {
-            Fruit fruit = new Fruit();
-
-            Assert.That(fruit, Is.Not.Null);
-        }
-
-        [Test]
-        public void Test_IsFruitObject()
-        {
-            var veggie = new Fruit();
-
-            Assert.That(veggie, Is.InstanceOf<Fruit>());
         }
     }
 }
---
+++ FinalExamProject/LogicEngineTests/RunTimeClassTests_FoodItems.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using LogicEngine.RunTimeClass;
+using LogicEngine.RunTimeClass.FoodItem;
+
+namespace LogicEngineTests
+{
+    public class RunTimeClassTests_FoodItems
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_VeggieObjectNotNull()
+        {
+            Veggie veggie = new Veggie();
+            
+            Assert.That(veggie, Is.Not.Null);
+        }
+
+        [Test]
+        public void Test_IsVeggieObject()
+        {
+            var veggie = new Veggie();
+
+            Assert.That(veggie, Is.InstanceOf<Veggie>());
+        }
+
+        [Test]
+        public void Test_FruitObjectNotNull()
+        {
+            Fruit fruit = new Fruit();
+
+            Assert.That(fruit, Is.Not.Null);
+        }
+
+        [Test]
+        public void Test_IsFruitObject()
+        {
+            var veggie = new Fruit();
+
+            Assert.That(veggie, Is.InstanceOf<Fruit>());
+        }
+    }
+}
