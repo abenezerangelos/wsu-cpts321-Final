80e3b66a2b5a80572e91ae372395320180aa4fa7
nkarl <21993921+nkarl@users.noreply.github.com>
2022-04-29 15:24:34 -0700

    Added more tests for basket objects

--- FinalExamProject/LogicEngineTests/RunTimeClassTests_Baskets.cs
+++ FinalExamProject/LogicEngineTests/RunTimeClassTests_Baskets.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using LogicEngine.RunTimeClass;
-
+using LogicEngine.RunTimeClass.FoodItem;
+using LogicEngine;
 
 namespace LogicEngineTests
 {
@@ -11,6 +12,34 @@ namespace LogicEngineTests
         {
         }
 
+        [Test]
+        public void Test_FruitBasketIsEmpty()
+        {
+            var basket = new Basket<Fruit>();
 
+            basket.Add(new Fruit());
+
+            Assert.That(basket, Is.Not.Null);
+        }
+
+        [Test]
+        public void Test_FruitBasketIsNotNull()
+        {
+            var basket = new Basket<Fruit>();
+
+            basket.Add(new Fruit());
+
+            Assert.That(basket.Contents.Count, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void Test_FruitBasketContentIsFruit()
+        {
+            var basket = new Basket<Fruit>();
+
+            basket.Add(new Fruit());
+
+            Assert.That(basket.Contents[0], Is.InstanceOf<Fruit>());
+        }
     }
 }
