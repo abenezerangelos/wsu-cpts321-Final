927ca237c77479b1cddd34f84dbcdfac2bfbad7b
nkarl <21993921+nkarl@users.noreply.github.com>
2022-05-01 20:34:24 -0700

    Added some code for the console app to generate output for item attributes

--- FinalExamProject/FinalExamConsoleApp/Program.cs
+++ FinalExamProject/FinalExamConsoleApp/Program.cs
@@ -1,4 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using LogicEngine;
+using LogicEngine.ItemAttributes;
+using LogicEngine.AbstractBaseClass;
+using LogicEngine.RunTimeClass.FoodItems;
+
 Console.WriteLine("Hello, Venera!");
 
+var application = new EngineController();
+
+application.CreateFoodItem("Veggie");
+Veggie veggie1 = (Veggie)application.ItemContainer[0];
+veggie1.Name = "Lettuce";
+veggie1.EnumColor = ItemColor.GREEN;
+veggie1.EnumShape = ItemShape.SQUARE;
+veggie1.EnumTaste = ItemTaste.SWEET;
+veggie1.EnumTexture = ItemTexture.CRUNCHY;
+veggie1.EnumSize = ItemSize.MEDIUM;
+
+application.CreateFoodItem("Fruit");
+Fruit fruit1 = (Fruit)application.ItemContainer[1];
+fruit1.Name = "Banana";
+fruit1.EnumColor = ItemColor.YELLOW;
+fruit1.EnumShape = ItemShape.SQUARE;
+fruit1.EnumTaste = ItemTaste.SWEET;
+fruit1.EnumTexture = ItemTexture.SOFT;
+fruit1.EnumSize = ItemSize.YUGE;
+
+application.CreateBasket("Veggie");
+var veggieBasket1 = application.BasketContainer[0];
+
+application.CreateBasket("Fruit");
+var fruitBasket1 = application.BasketContainer[1];
+
+/// <summary>
+/// 
+/// </summary>
+static void PrintItemAttributes(Food item)
+{
+    Console.WriteLine($"ITEM ATTRIBUTES FOR THE FOOD ITEM {item.Name}");
+    Console.WriteLine($"	{item.Color}");
+    Console.WriteLine($"	{item.Shape}");
+    Console.WriteLine($"	{item.Size}");
+    Console.WriteLine($"	{item.Texture}");
+    Console.WriteLine($"	{item.Taste}");
+}
+
+PrintItemAttributes((Food)veggie1);
+PrintItemAttributes((Food)fruit1);
 
--- FinalExamProject/FinalExamProject/Form1.cs
+++ FinalExamProject/FinalExamProject/Form1.cs
@@ -4,17 +4,23 @@
 
 namespace FinalExamProject
 {
+    using LogicEngine;
+
     /// <summary>
     /// Windows Form.
     /// </summary>
     public partial class Form1 : Form
     {
+        EngineController application;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Form1"/> class.
         /// </summary>
         public Form1()
         {
             this.InitializeComponent();
+
+            this.application = new EngineController();
         }
 
         private void CheckedListBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -23,6 +29,16 @@ namespace FinalExamProject
 
         private void ListView2_SelectedIndexChanged(object sender, EventArgs e)
         {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
