f7efafc13f309737484e65eeede0207b7d2dfad6
nkarl <21993921+nkarl@users.noreply.github.com>
2022-05-01 20:57:17 -0700

    mnior changes to form dimensions

--- FinalExamProject/FinalExamProject/Form1.Designer.cs
+++ FinalExamProject/FinalExamProject/Form1.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxForBaskets = new System.Windows.Forms.ListBox();
+            this.listBoxForItems = new System.Windows.Forms.ListBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.listBoxForItems = new System.Windows.Forms.ListBox();
-            this.listBoxForBaskets = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -54,7 +54,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1378, 772);
+            this.tabControl1.Size = new System.Drawing.Size(1347, 766);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -71,10 +71,93 @@
             this.tabPage1.Location = new System.Drawing.Point(8, 46);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1362, 718);
+            this.tabPage1.Size = new System.Drawing.Size(1331, 712);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(231, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(219, 93);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "RemoveItemFromBasket";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(973, 8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(219, 93);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "DeleteBasket";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(219, 93);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "DeleteFoodItem";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(456, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(219, 95);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "AddItemToBasket";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(231, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(219, 95);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "ChangeExistingItem";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(748, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 95);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "CreateBasket";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 95);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CreateFoodItem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBoxForBaskets
+            // 
+            this.listBoxForBaskets.FormattingEnabled = true;
+            this.listBoxForBaskets.ItemHeight = 32;
+            this.listBoxForBaskets.Location = new System.Drawing.Point(748, 221);
+            this.listBoxForBaskets.Name = "listBoxForBaskets";
+            this.listBoxForBaskets.Size = new System.Drawing.Size(575, 484);
+            this.listBoxForBaskets.TabIndex = 1;
+            // 
+            // listBoxForItems
+            // 
+            this.listBoxForItems.FormattingEnabled = true;
+            this.listBoxForItems.ItemHeight = 32;
+            this.listBoxForItems.Location = new System.Drawing.Point(6, 221);
+            this.listBoxForItems.Name = "listBoxForItems";
+            this.listBoxForItems.Size = new System.Drawing.Size(575, 484);
+            this.listBoxForItems.TabIndex = 0;
+            this.listBoxForItems.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
             // 
             // tabPage2
             // 
@@ -96,94 +179,11 @@
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // listBoxForItems
-            // 
-            this.listBoxForItems.FormattingEnabled = true;
-            this.listBoxForItems.ItemHeight = 32;
-            this.listBoxForItems.Location = new System.Drawing.Point(6, 221);
-            this.listBoxForItems.Name = "listBoxForItems";
-            this.listBoxForItems.Size = new System.Drawing.Size(575, 484);
-            this.listBoxForItems.TabIndex = 0;
-            this.listBoxForItems.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
-            // 
-            // listBoxForBaskets
-            // 
-            this.listBoxForBaskets.FormattingEnabled = true;
-            this.listBoxForBaskets.ItemHeight = 32;
-            this.listBoxForBaskets.Location = new System.Drawing.Point(748, 221);
-            this.listBoxForBaskets.Name = "listBoxForBaskets";
-            this.listBoxForBaskets.Size = new System.Drawing.Size(529, 484);
-            this.listBoxForBaskets.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(6, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 95);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "CreateFoodItem";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(748, 8);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(219, 95);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "CreateBasket";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(231, 6);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(219, 95);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "ChangeExistingItem";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(456, 6);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(219, 95);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "AddItemToBasket";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(6, 107);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(219, 93);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "DeleteFoodItem";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(973, 8);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(219, 93);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "DeleteBasket";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(231, 107);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(219, 93);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "RemoveItemFromBasket";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1378, 772);
+            this.ClientSize = new System.Drawing.Size(1347, 766);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
